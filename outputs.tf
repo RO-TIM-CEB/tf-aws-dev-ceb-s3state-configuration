e2179d95781a402dc998f20ca03e11fc4436e077
Alexandru Raul <raul.alexandru@atos.net>
2023-09-22 22:24:14 +0300

    change

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # Export the bucket name as an output variable
 output "terraform_state_bucket_name" {
   description = "Terraform TF state S3 bucket name"
-  value = module.terraform_state_bucket_name
+  value = module.s3_state_file.terraform_state_bucket_name.id
 }
