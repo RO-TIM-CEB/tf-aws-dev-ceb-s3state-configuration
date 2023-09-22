79f2e2499ef2a7ebb9b2f58f940e3af57f8493e8
Alexandru Raul <raul.alexandru@atos.net>
2023-09-22 23:13:02 +0300

    changed

--- provider.tf
+++ provider.tf
@@ -1,8 +1,22 @@
-provider "github" {
- source = "integrations/github"
- version = "~> 5.0"
+# Author: Alexandru Raul
+terraform {
+  required_version = "~> 1.5.2"  # Specify the minimum required Terraform version here
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "~> 5.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"  # Specify the desired version constraint for AWS provider here
+    }
+  }
 }
 
-terraform {
-   required_version = "~> 1.5.2"  # Specify the minimum required Terraform version here
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+
 }
+
+# Define your AWS resources and configurations below this point
