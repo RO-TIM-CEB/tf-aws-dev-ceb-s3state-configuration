2fa7d2f429d6310dfe1d0f302b26ace10da47bf4
Alexandru Raul <raul.alexandru@atos.net>
2023-09-22 21:45:01 +0300

    change module

--- main.tf
+++ main.tf
@@ -1,45 +1,47 @@
 module "s3_state_file" {
   source = "git@github.com:RO-TIM-CEB/tf-aws-dev-ceb-s3state-module.git?ref=v0.2.0"
 
+# terraform.tfvars
 # Important note:
 # If you want to change optional values, you must know what you are doing.
+
 
 #Global variables
 # MANDATORY
 # Default region
-  region                                      = var.region       # Allowed regions: us-west-2, eu-west-1 and eu-central-1
-# End Default region
-# Default region
-  terraform_module_version                    = var.terraform_module_version    # Allowed characters: vX.X.X, lower case
+  region                    = "us-west-2"            # Allowed regions: us-west-2, eu-west-1 and eu-central-1
 # End Default region
 # AWS profile
-  profile                                     = var.profile      # Allowed profile names: ceb-dev, ceb-staging and ceb-prod
+  profile                    = "production"            # Allowed profile names: ceb-dev, ceb-staging and ceb-prod
 # End AWS profile
+terraform_module_version     = "v0.2.0"
 # End MANDATORY
 # End Global variables
 
 
 # Terraform S3 variable
 # MANDATORY
-  s3_bucket_name            = var.s3_bucket_name # Maximum 30 characters, no defaults, RECOMMANDATION: If you have the same name as DynamoDB table you'll know on what this bucket will be linked at
+  s3_bucket_name            = "infrastructure-state" # Maximum 30 characters, no defaults, RECOMMANDATION: If you have the same name as DynamoDB table you'll know on what this bucket will be linked at
 # End MANDATORY
 # OPTIONAL - ONLY IF YOU KNOW WHAT ARE YOU DOING!!!
-  s3_bucket_versioning      = var.s3_bucket_versioning                 # Allowed options Enabled/Disabled, defaults=Enabled
-  s3_server_side_encryption = var.s3_server_side_encryption            # Allowed options AES256/aws:kms/aws:kms:dsse, defaults=AES256
-  s3_block_public_acls      =  var.s3_block_public_acls                # Allowed options true/false, defaults=true
-  s3_block_public_policy    =  var.s3_block_public_policy              # Allowed options true/false, defaults=true
+
+  s3_bucket_versioning      = "Enabled"           # Allowed options Enabled/Disabled, defaults=Enabled
+  s3_server_side_encryption = "AES256"            # Allowed options AES256/aws:kms/aws:kms:dsse, defaults=AES256
+  s3_block_public_acls      =  "true"               # Allowed options true/false, defaults=true
+  s3_block_public_policy    =  "true"               # Allowed options true/false, defaults=true
 # End OPTIONAL - ONLY IF YOU KNOW WHAT ARE YOU DOING!!!
 # End Terraform S3 variable
 
+
 # Terraform DynamoDB table variable
 # MANDATORY
-  dynamo_tbl_name                    =  var.dynamo_tbl_name # Maximum 30 characters, no defaults, RECOMMANDATION: If you have the same name as S3 bucket you'll know what this table is for
+  dynamo_tbl_name                    = "infrastructure-state" # Maximum 30 characters, no defaults, RECOMMANDATION: If you have the same name as S3 bucket you'll know what this table is for
 # End MANDATORY
 # OPTIONAL - ONLY IF YOU KNOW WHAT ARE YOU DOING!!!
- dynamo_tbl_attribute_type            = var.dynamo_tbl_attribute_type             # Allowed options S/N/B, defaults=S
- dynamo_tbl_hash_key                  = var.dynamo_tbl_hash_key          # Allowed options LockID, defaults=LockID
- dynamo_tbl_billing_mode              = var.dynamo_tbl_billing_mode # Allowed options PAY_PER_REQUEST/PROVISIONED, defaults=PAY_PER_REQUEST
- dynamo_tbl_point_in_time_recovery    = var.dynamo_tbl_point_in_time_recovery              # Allowed options true/false, defaults=true
+ dynamo_tbl_attribute_type            = "S"               # Allowed options S/N/B, defaults=S
+ dynamo_tbl_hash_key                  = "LockID"          # Allowed options LockID, defaults=LockID
+ dynamo_tbl_billing_mode              = "PAY_PER_REQUEST" # Allowed options PAY_PER_REQUEST/PROVISIONED, defaults=PAY_PER_REQUEST
+ dynamo_tbl_point_in_time_recovery    = "true"              # Allowed options true/false, defaults=true
 # End OPTIONAL - ONLY IF YOU KNOW WHAT ARE YOU DOING!!!
 # End Terraform DynamoDB table variable
 
@@ -48,25 +50,87 @@ module "s3_state_file" {
 #### Ensure all code is written in lowercase (lowcaps) as it is MANDATORY.
 #### Failure to do so during execution can result in the code breaking the standard and causing the execution to fail.
 #### Please adhere to this requirement to maintain consistency and avoid issues.
-
 # Business TAGS
-  business_tags               = var.business_tags
-# End Business TAGS
+  business_tags = {
+    "atos:business:project"                  = "ceb development - ceb on aws"
+    "atos:business:owner"                    = "ceb team"
+    "atos:business:charge_to_id"             = "none"
+    "atos:business:businessunit"             = "businessunit"
+    "atos:business:wbs"                      = "wbs"
+    "atos:business:stakeholder"              = "atos"
+    "atos:business:impact"                   = "low"
+    "atos:business:dedicated:client_name"    = "ceb"
+    "atos:business:dedicated:country"        = "all"
+  }
+  # End Business TAGS
 
 # Technical TAGS
-  technical_s3_tags           = var.technical_s3_tags
-  technical_dynamodbtbl_tags  = var.technical_dynamodbtbl_tags
-# End Business TAGS
+# S3 technical TAGS
+  technical_s3_tags = {
+   "atos:s3:technical:stack"                     = "development"
+   "atos:s3:technical:deployment_method"         = "terraform"
+   "atos:s3:technical:versioning"               = "enabled"
+   "atos:s3:technical:replication"               = "disabled"
+   "atos:s3:technical:lifecycle"                 = "disabled"
+   "atos:s3:technical:logging"                   = "disabled"
+   "atos:s3:technical:notification"              = "none"
+  }
+# End S3 technical TAGS
+
+# Dynamodb table  technical TAGS
+technical_dynamodbtbl_tags = {
+   "atos:dynamotbl:technical:stack"                     = "development"
+   "atos:dynamotbl:technical:deployment_method"         = "terraform"
+   "atos:dynamotbl:technical:versioning"               = "enabled"
+   "atos:dynamotbl:technical:replication"               = "disabled"
+   "atos:dynamotbl:technical:lifecycle"                 = "disabled"
+   "atos:dynamotbl:technical:logging"                   = "disabled"
+   "atos:dynamotbl:technical:notification"              = "none"
+}
+# End Dynamodb table  technical TAGS
+# End Technical TAGS
 
 # Security TAGS
-  security_s3_tags            = var.security_s3_tags
-  security_dynamotbl_tags     = var.security_dynamotbl_tags
+#Dynamodb table security TAGS
+security_dynamotbl_tags = {
+  "atos:dynamotbl:security:compliance"            = "none"
+  "atos:dynamotbl:security:classification"        = "restricted"
+  "atos:dynamotbl:security:encryption"            = "unencrypted"
+  "atos:dynamotbl:security:level"                 = "low"
+  "atos:dynamotbl:security:incident_response"     = "adrian mendyk"
+  "atos:dynamotbl:security:access_control"        = "private access"
+}
+# End Dynamodb table security TAGS
+
+# S3 security TAGS
+security_s3_tags = {
+  "atos:s3:security:compliance"             = "none"
+  "atos:s3:security:classification"         = "restricted"
+  "atos:s3:security:encryption"             = "encrypted"
+  "atos:s3:security:level"                  = "medium"
+  "atos:s3:security:incident_response"      = "adrian mendyk"
+  "atos:s3:security:access_control"         = "private access"
+}
+# End S3 security TAGS
 # End Security TAGS
 
 # Billing TAGS
-  billing_tags                = var.billing_tags
-# End Billing TAGS
+billing_tags = {
+  "atos:billing:credit"                   = "apn_12_21_2022"
+  "atos:billing:account_name"             = "737580263478"
+  "atos:billing:contact_name"             = "radoslaw wieckowski"
+  "atos:billing:contact_email"            = "radoslaw.wieckowski@atos.net"
+}
+ # End Billing TAGS
+
 # Backup S3 TAGS
-  backup_s3_tags              = var.backup_s3_tags
-# End Backup S3 TAGS
+backup_s3_tags = {
+  "atos:backup:s3"                    = "backup"
+  "atos:backup:charge_to_id"          = "a hci del prihybclo som"
+  "atos:backup:frequency"             = "none"
+  "atos:backup:owner"                 = "ceb"
+  "atos:backup:data_classification"   = "confidential"
+}
+# End Backup TAGS
+# End  MANDATORY
 }
